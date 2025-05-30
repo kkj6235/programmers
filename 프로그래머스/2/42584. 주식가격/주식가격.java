d4643248d2eeb2f746a787c08ea17ffa1b0d268e
Gwan Ju <113610239+kkj6235@users.noreply.github.com>
2025-05-30 10:00:00 +0900

    [level 2] Title: 주식가격, Time: 47.56 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i=0;i<prices.length;i++){
+            while(!stack.isEmpty() && prices[i] < prices[stack.peek()]){
+                // pop 시키고 저장
+                answer[stack.peek()] = i - stack.peek();
+                stack.pop();
+            }
+            stack.push(i);
+        }
+        
+        while(!stack.isEmpty()){
+            answer[stack.peek()] = prices.length-1 - stack.peek();
+            stack.pop();
+        }
+        
+        return answer;
+    }
+}
