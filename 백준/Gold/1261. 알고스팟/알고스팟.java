6e5fa322a18b470d4b6375308d8bece9f0921532
Gwan Ju <113610239+kkj6235@users.noreply.github.com>
2025-11-14 10:00:00 +0900

    [Gold IV] Title: 알고스팟, Time: 320 ms, Memory: 96600 KB -BaekjoonHub

---
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -0,0 +1,102 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static StringTokenizer st;
+    public static BufferedReader br;
+    public static int N,M,answer;
+    public static boolean[][] visited, open;
+    public static int[][] sum;
+
+    public static void main(String[] args) throws IOException{
+        
+        br = new BufferedReader(new InputStreamReader(System.in));
+
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[M][N];
+        open = new boolean[M][N];
+        sum = new int[M][N];
+
+        for(int i=0;i<M;i++){
+            String tmp = br.readLine();
+            for(int j=0;j<N;j++){
+                if(tmp.charAt(j)=='0'){
+                    open[i][j] = true;
+                }
+            }
+            Arrays.fill(sum[i], Integer.MAX_VALUE);
+        }
+
+        
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(0,0,0));
+        
+        int[] dx = {-1,1,0,0};
+        int[] dy = {0,0,-1,1};
+        answer = Integer.MAX_VALUE;
+
+        while(!queue.isEmpty()){
+            Node tmp = queue.poll();   
+            
+            if(tmp.x ==M-1 && tmp.y ==N-1){
+                if(tmp.count<answer){
+                    answer = tmp.count;                    
+                }
+                continue;
+            }
+            // 방문 했어도 count가 낮으면 계속 시도
+
+            if(visited[tmp.x][tmp.y]){
+                if(tmp.count>=sum[tmp.x][tmp.y]){
+                    continue;
+                }
+            }
+
+            // System.out.println(tmp.x+" "+tmp.y+" "+tmp.count);
+            visited[tmp.x][tmp.y]=true;
+            sum[tmp.x][tmp.y] = tmp.count;
+
+            for(int i=0;i<4;i++){
+                int tmpX = tmp.x+dx[i];
+                int tmpY = tmp.y+dy[i];
+                if(tmpX<0 || tmpX>=M || tmpY<0 || tmpY>=N){
+                    continue;
+                }
+                int count = open[tmpX][tmpY] ? tmp.count: tmp.count+1;
+                queue.add(new Node(tmpX, tmpY, count));
+            }
+
+        }
+
+        System.out.println(answer);
+    }
+
+    
+
+    public static class Node{
+        int x;
+        int y;
+        int count;
+
+        Node(int x, int y, int count){
+            this.x = x;
+            this.y = y;
+            this.count = count;
+        }
+
+        // implements Comparable<Node>
+
+        // @Override
+        // public int compareTo(Node n){
+        //     return Long.compare(this.distance, n.distance);
+        // }
+    }
+
+
+}
