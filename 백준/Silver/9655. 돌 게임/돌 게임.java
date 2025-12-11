4ddffeafe8b69b4b474aca48fe156d5ad8a5f309
Gwan Ju <113610239+kkj6235@users.noreply.github.com>
2025-12-11 10:00:00 +0900

    [Silver V] Title: 돌 게임, Time: 104 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+
+
+public class Main{
+
+    public static void main(String args[]) throws IOException{
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+
+        int [] dp = new int[1001];
+
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=1;
+
+        for(int i=4;i<1001;i++){
+            dp[i]= Math.min(dp[i-1], dp[i-3])+1;
+        }
+        
+        int n = Integer.parseInt(br.readLine());
+
+        
+
+        if (dp[n] % 2 == 0) {
+            System.out.println("CY");
+        } else {
+            System.out.println("SK");
+        }
+        
+    }
+}
+
